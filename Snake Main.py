84bf9274fbd5c181a4a65386d2c3690b1591dcd3
Kempu33334 <aatmikpk@gmail.com>
2025-02-21 20:25:09 -0600

    Add files via upload

---
+++ Snake Main.py
@@ -0,0 +1,99 @@
+import pygame
+import time
+import random
+import math
+
+pygame.init()
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption("Snake")
+
+background = pygame.image.load('Snake Background.png')
+
+length_snake = 5
+playerX = 200
+playerY = 200
+playerIMG = pygame.image.load('blue-square.png')
+snakeX = []
+snakeY = []
+playerIMGlist = []
+lastkey = "0"
+
+for i in range(length_snake-1):
+    snakeX.append(200-(20*i))
+    snakeY.append(200)
+    playerIMGlist.append(playerIMG)
+
+appleX = 20*round(random.randint(0,380)/20)
+appleY = 20*round(random.randint(0,380)/20)
+
+while math.sqrt(math.pow(playerX-appleX,2)+math.pow(playerY-appleY,2)) < 5:
+    appleX = 20*round(random.randint(0,380)/20)
+    appleY = 20*round(random.randint(0,380)/20)
+
+running = True
+while running:
+    screen.blit(background,(0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                if lastkey != "right":
+                    lastkey = "left"
+            if event.key == pygame.K_RIGHT:
+                if lastkey != "left":
+                    lastkey = "right"
+            if event.key == pygame.K_UP:
+                if lastkey != "down":
+                    lastkey = "up"
+            if event.key == pygame.K_DOWN:
+                if lastkey != "up":
+                    lastkey = "down"
+    if lastkey != "0":
+        for j in range(length_snake-2):
+            snakeX[length_snake-j-2] = snakeX[length_snake-j-3]
+            snakeY[length_snake-j-2] = snakeY[length_snake-j-3]
+        if lastkey == "left":
+            playerX -= 20
+            playerY = 20*round(playerY/20)
+        if lastkey == "right":
+            playerX += 20
+            playerY = 20*round(playerY/20)
+        if lastkey == "up":
+            playerY -= 20
+            playerX = 20*round(playerX/20)
+        if lastkey == "down":
+            playerY += 20
+            playerX = 20*round(playerX/20)
+        for t in range(len(snakeX)-1):
+            if playerX == snakeX[t] and playerY == snakeY[t]:
+                running = False
+    snakeX[0] = playerX
+    snakeY[0] = playerY
+    if playerX > 380:
+        playerX = 380
+        break
+    if playerX < 0:
+        playerX = 0
+        break
+    if playerY > 380:
+        playerY = 380
+        break
+    if playerY < 0:
+        playerY = 0
+        break
+    if playerX == appleX and playerY == appleY:
+        appleX = 20*round(random.randint(0,380)/20)
+        appleY = 20*round(random.randint(0,380)/20)
+        while math.sqrt(math.pow(playerX-appleX,2)+math.pow(playerY-appleY,2)) < 5:
+            appleX = 20*round(random.randint(0,380)/20)
+            appleY = 20*round(random.randint(0,380)/20)
+        length_snake = length_snake + 1
+        snakeX.append(snakeX[-1])
+        snakeY.append(snakeY[-1])
+        playerIMGlist.append(playerIMG)
+    for g in range(length_snake-1):
+        screen.blit(playerIMGlist[g], (snakeX[g], snakeY[g]))
+    screen.blit(pygame.image.load('apple.jpg'),(appleX,appleY))
+    pygame.display.update()
+    time.sleep(0.1)
